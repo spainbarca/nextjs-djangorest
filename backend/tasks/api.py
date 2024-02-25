bef5c23c20c7d8da2a1a052cd1137f8f669edb7c
Noah <practicasjaguar@gmail.com>
2024-02-25 18:58:16 -0500

    Task done undone

--- backend/tasks/api.py
+++ backend/tasks/api.py
@@ -1,8 +1,19 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     permission_clases = [permissions.AllowAny]
     serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['post'])
+    def done(self, request, pk=None):
+        task = self.get_object()
+        task.done = not task.done
+        task.save()
+        return Response({
+            'status': 'task done' if task.done else 'task undone'
+            },status=status.HTTP_200_OK)
