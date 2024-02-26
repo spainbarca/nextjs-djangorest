3a93eefc88a31adf7dce470564bbd005aa28bd97
Noah <practicasjaguar@gmail.com>
2024-02-26 00:24:58 -0500

    Añadir tareas y listarlas

--- frontend/src/app/components/FormTask.jsx
+++ frontend/src/app/components/FormTask.jsx
@@ -1,16 +1,39 @@
+"use client"
+
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
 function FormTask() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('');
+    const router = useRouter()
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        console.log(title, description)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/task/`, {
+            method: "POST",
+            body: JSON.stringify({title, description}),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        const data = await res.json()
+        console.log(data)
+        router.refresh()
+    };
 
     return (
-        <div className="bg-slate-200 p-7">
-            <form action="">
-            <h1 className="text-black">Añadir Tarea</h1>
+        <div className="bg-slate-200 p-7 h-fit">
+            <form onSubmit={handleSubmit}>
+            <h2 className="text-black font-bold">Añadir Tarea</h2><br />
             <div class="mb-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Tarea:</label>
-                <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"   placeholder="Nombre de la tarea" required />
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Tarea:</label>
+                <input type="text" id="title" name="title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Nombre de la tarea" onChange={e => setTitle(e.target.value)} required />
             </div>
             <div class="mb-5">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Descripción:</label>
-                <textarea type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Descripción detallada de la tarea" required />
+                <textarea type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Descripción detallada de la tarea" onChange={e => setDescription(e.target.value)} required />
             </div>
             
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full">Guardar</button>
--- frontend/src/app/components/ListTask.jsx
+++ frontend/src/app/components/ListTask.jsx
@@ -1,9 +1,33 @@
-function ListTask() {
-    
-
-    return (
-        <div>ListTask</div>
-    )
+async function loadTasks() {
+    const res = await fetch(`${process.env.BACKEND_URL}/api/task/`);
+    const tasks = await res.json();
+    return tasks;
 }
 
-export default ListTask
+async function ListTask() {
+    const tasks = await loadTasks();
+    console.log(tasks);
+    return (
+        <div className="bg-slate-700 p-4 w-full">
+            <h1>Lista de tareas</h1>
+            {tasks.map((task) => (
+                <div key={task.id} className="bg-slate-500 px-4 py-3 mb-2 rounded-md flex justify-between items-center">
+                    <div>
+                        <h2>{task.title}</h2>
+                        <p>{task.description}</p>
+                    </div>
+                    <div class="inline-flex gap-x-2">
+                        <button class="bg-yellow-300 hover:bg-yellow-400 text-yellow-800 font-bold py-2 px-4 rounded-l">
+                            Editar
+                        </button>
+                        <button class="bg-red-300 hover:bg-red-400 text-red-800 font-bold py-2 px-4 rounded-r">
+                            Eliminar
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ListTask;
